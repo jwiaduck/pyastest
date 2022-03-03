ca6286b21340920ae222b2849a92bbea9e5f8f46
jwiaduck <jwiaduck@umich.edu>
2022-03-03 01:05:37 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+pyastest
+===========================
+a command line tool to compare Python ASTs
+
+# Quick start
+Note: Developed with Python 3.9.10 .
+Note: Rudimentally tested. Proceed with caution!
+
+```console
+$ git clone <this repo>
+$ cd pyastest
+$ python3 pyastest.py path/to/file1 path/to/file2
+```
+
+I plan to build both this comparison tool as well as other tools utilizing Abstract Syntax Trees in this repository.
---
+++ pyastest.py
@@ -0,0 +1,81 @@
+# pyastest:
+#   a tool to compare Python ASTs
+# 
+# @jwiaduck 3 March 2022
+#
+
+import ast
+import sys
+
+
+###
+### Visitor Subclass
+###
+
+class MyVisitor(ast.NodeVisitor):
+
+    def generic_visit(self, node):
+        # print("Visitor sees a node: ", ast.dump(node) ))
+        # print(f'Nodetype: {type(node).__name__:{16}} {node}')
+        ast.NodeVisitor.generic_visit(self, node)
+
+
+###
+### Helpers
+###
+
+def parseAst(source):
+    tree = ast.parse(source)
+    ast.fix_missing_locations(tree)
+    return tree
+
+def compileAst(tree_in):
+    exec(compile(tree_in, filename="<ast>", mode="exec"))
+
+def unparseAst(tree_in):
+    print(ast.unparse(tree_in))
+
+def getAst(source):
+    tree = parseAst(source)
+    print("Got AST!\n")
+    return tree
+
+
+###
+### Mains .-.
+###
+
+def main():
+
+    if len(sys.argv) != 3:
+        print("Usage: python3 pyatest.py path/to/file1 path/to/file2.")
+        exit(0)
+
+    print("Starting...\n")
+
+    file_1 = sys.argv[1]
+    with open(file_1, encoding='utf-8') as f:
+        src1 = f.read()
+    f.close()
+
+    file_2 = sys.argv[2]
+    with open(file_2, encoding='utf-8') as f:
+        src2 = f.read()
+    f.close()
+
+    tree_1 = getAst(src1)
+    tree_2 = getAst(src2)
+    
+    if ast.dump(tree_1) == ast.dump(tree_2):
+        print("True: ASTs are equal!")
+    else:
+        print("False: Not equal.")
+    print("Done!")
+    exit(1)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
