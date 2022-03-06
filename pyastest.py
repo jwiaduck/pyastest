f4b7ba416203227a3ad7023589ee1f607f1a22be
jwiaduck <jwiaduck@umich.edu>
2022-03-06 00:28:07 -0500

    modulated ast_diff

--- pyastest.py
+++ pyastest.py
@@ -9,39 +9,32 @@ import ast
 import sys
 import os
 
-###
-### Visitor Subclass
-###
-
-class MyVisitor(ast.NodeVisitor):
-
-    def generic_visit(self, node):
-        # print("Visitor sees a node: ", ast.dump(node) ))
-        # print(f'Nodetype: {type(node).__name__:{16}} {node}')
-        ast.NodeVisitor.generic_visit(self, node)
-
 
 ###
 ### Helpers
 ###
 
+# parses source code to AST
 def parse_ast(source):
     tree = ast.parse(source)
     ast.fix_missing_locations(tree)
     return tree
 
+# compiles and executes AST
 def exec_co_ast(tree_in):
     exec(compile(tree_in, filename="<ast>", mode="exec"))
 
+# ast.py unparse method
 def unparse_ast(tree_in):
     print(ast.unparse(tree_in))
 
+# agg. fxn to call AST helpers - namely, get_ast (and return tree)
 def get_ast(source):
     tree = parse_ast(source)
     print("Got AST!")
     return tree
 
-
+# checks that file is read/write-able
 def r_w_file(file):
     if os.path.exists(file):
         if os.access(file, os.W_OK and os.R_OK):
@@ -53,6 +46,7 @@ def r_w_file(file):
         print('The specified file does not exist')
         exit(0)
 
+# command line argument parsing
 def parse_arguments():
     parser = argparse.ArgumentParser(prog='pyastest')
     parser.add_argument('original', metavar='FILE_1', type=r_w_file,
@@ -70,16 +64,7 @@ def parse_arguments():
     return args
 
 
-###
-### Mains .-.
-###
-
-def main():
-
-    args = parse_arguments() 
-
-    print("Starting...\n")
-
+def ast_diff(args):
     file_1 = args.original
     with open(file_1, encoding='utf-8') as f:
         src1 = f.read()
@@ -92,12 +77,25 @@ def main():
     print(f"Src Original: {file_1}\nSrc Changed: {file_2}")
     tree_1 = get_ast(src1)
     tree_2 = get_ast(src2)
-    
+
     if ast.dump(tree_1) == ast.dump(tree_2):
         print("\nTrue: ASTs are equal!")
     else:
         print("\nFalse: Not equal.")
-    print("Finished!")
+
+
+###
+### Mains .-.
+###
+
+def main():
+
+    args = parse_arguments() 
+
+    print("Starting...\n")
+
+    ast_diff(args)
+
     exit(1)
 
 
